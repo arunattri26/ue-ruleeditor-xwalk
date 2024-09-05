36eb8a30468e8e01a5e3e094d9483f3fd62c0c53
Arun Kumar Attri <aattri@adobe.com>
2024-09-05 16:38:06 +0530

    Unified Onboarding: ruleeditor-ui-service ready for prod

--- scripts/form-editor-support.js
+++ scripts/form-editor-support.js
@@ -227,8 +227,8 @@ function enableRuleEditorExtension() {
   meta.name = 'urn:adobe:aue:config:extensions';
   // meta.content = 'https://283250-aattriformsueextnsn-stage.adobeio-static.net';
   // meta.content = 'https://283250-aattriformsueextnsn-stage.dev.runtime.adobe.io';
-  // meta.content = 'https://experience-stage.adobe.com/solutions/livecycle-ruleeditor-ui-service/static-assets/resources/universal_editor.html?livecycle-ruleeditor-ui-service_version=PR-43-bbeab28e12cc1b86de9c6230aa67ccad9317a8d5';
-  meta.content = 'https://author-p63964-e255534-cmstg.adobeaemcloud.com/ui/solutions/livecycle-ruleeditor-ui-service/static-assets/resources/universal_editor.html?livecycle-ruleeditor-ui-service_version=PR-43-5f1d9a71a541a111d2dba64b845beadd996bee2e';
+  meta.content = 'https://experience-stage.adobe.com/solutions/livecycle-ruleeditor-ui-service/static-assets/resources/universal_editor.html?livecycle-ruleeditor-ui-service_version=PR-45-4e43d98883b8d39b34997f4edfb00216a3bffec1';
+  // meta.content = 'https://author-p63964-e255534-cmstg.adobeaemcloud.com/ui/solutions/livecycle-ruleeditor-ui-service/static-assets/resources/universal_editor.html?livecycle-ruleeditor-ui-service_version=PR-43-5f1d9a71a541a111d2dba64b845beadd996bee2e';
   head.appendChild(meta);
 }
 
